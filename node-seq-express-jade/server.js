cef1bf3ce28fda3c9a9863fa31690398cb1bc1be
Dmitry Frolov <goldtool@gmail.com>
2015-04-24 17:40:00 +0300

    add clusters

--- node-seq-express-jade/server.js
+++ node-seq-express-jade/server.js
@@ -1,55 +1,71 @@
 'use strict';
 
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('infiniti', 'root', '!QAZ2wsx', {
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    pool: {
-        maxConnections: 5,
-        maxIdleTime: 30
-    },
-    define: {
-        timestamps: false,
-        freezeTableName: true,
-        underscored: false,
-        syncOnAssociation: false
-    }
-});
-
-var Title = sequelize.define('Title', {
-    TitleID: {
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
-    TitleName: Sequelize.STRING
-});
-
 var express = require('express');
 var path = require('path');
 var jade = require('jade');
-var app = express();
 
-app.set('view engine', 'jade');
-app.set('views', path.normalize(__dirname + '/public'));
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-var indexJadeTmp = jade.compileFile('./public/index.jade');
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case its a HTTP server
 
-function extractItems() {
-    return Title.findAll();
+
+    var sequelize = new Sequelize('infiniti', 'root', '!QAZ2wsx', {
+        dialect: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        pool: {
+            maxConnections: 5,
+            maxIdleTime: 30
+        },
+        define: {
+            timestamps: false,
+            freezeTableName: true,
+            underscored: false,
+            syncOnAssociation: false
+        }
+    });
+
+    var Title = sequelize.define('Title', {
+        TitleID: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        TitleName: Sequelize.STRING
+    });
+
+
+    var app = express();
+
+    app.set('view engine', 'jade');
+    app.set('views', path.normalize(__dirname + '/public'));
+
+    var indexJadeTmp = jade.compileFile('./public/index.jade');
+
+    app.get('/', function (req, res) {
+        Title.findAll().then(function (items) {
+            res.render('index', {items: items});
+        });
+    });
+
+    app.get('/compile', function (req, res) {
+        Title.findAll().then(function (items) {
+            res.send(indexJadeTmp({items: items}));
+        });
+    });
+
+    app.listen(3006);
+    console.log('app is running and listening 3006');
 }
-
-app.get('/', function (req, res) {
-    extractItems().then(function (items) {
-        res.render('index', {items: items});
-    });
-});
-
-app.get('/compile', function (req, res) {
-    extractItems().then(function (items) {
-        res.send(indexJadeTmp({items: items}));
-    });
-});
-
-app.listen(3006);
-console.log('app is running and listening 3006');
